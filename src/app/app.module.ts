0fd458fb00f0b0748ec713334347ab48d5d3373a
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-05-28 12:04:32 +0200

    responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import {MatInputModule} from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
+import { PlayerMobileComponent } from './player-mobile/player-mobile.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import {MatCardModule} from '@angular/material/card';
     GameInfoComponent,
     GameComponent,
     PlayerComponent,
-    StartScreenComponent
+    StartScreenComponent,
+    PlayerMobileComponent
   ],
   imports: [
     BrowserModule,
